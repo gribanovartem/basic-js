bee6514510ef2cb4e87fa543d375e72a1b7886b6
gribanovartem <gribanovartem22@gmail.com>
2020-09-27 10:41:22 +0300

    feat: implement transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,38 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if(!Array.isArray(arr)) {
+    throw new Error('THROWN');
+  }
+  let newArr = [];
+  for(let i=0; i<arr.length; i++) {
+    switch (arr[i]) {
+      case '--discard-next':
+        if(arr[i+2] ==='--discard-prev' || arr[i+2] ==='--double-prev') {
+          i += 2;
+        } else {
+          i++;
+        }
+        break;
+      case '--discard-prev':
+        if(newArr.length!==0) {
+          newArr.pop();
+        }
+        break;
+      case '--double-next':
+        if(typeof (arr[i+1])!=='undefined') {
+          newArr.push(arr[i+1]);
+        }
+      break;
+      case '--double-prev':
+        if(newArr.length!==0 && arr[i-2]!=='--discard-next') {
+          newArr.push(newArr[newArr.length-1]);
+        }
+      break;
+      default:
+        newArr.push(arr[i]);
+        break;
+    }
+  }
+  return newArr;
 };
