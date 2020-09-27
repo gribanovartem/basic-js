47c0dfcdde9fc07824bd15f90652311bb76dfbbd
gribanovartem <gribanovartem22@gmail.com>
2020-09-27 19:21:17 +0300

    feat: implement vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,71 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(bool = true) {
+    this.isRevert = !bool;
+    this.letters =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+  encrypt(message, key) {
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let keyStr = '';
+    let count = 0;
+    for(let j=0; j<message.length; j++) {
+      if(this.letters.indexOf(message[j])!==-1) {
+        keyStr += key[count];
+        count++;
+      } else {
+        keyStr += message[j];
+      }
+      if(count===key.length) {
+        count = 0;
+      }
+    }
+    let decryptStr = '';
+    for(let i=0; i<message.length; i++) {
+      let char;
+      if(this.letters.indexOf(message[i])!==-1) {
+        char = (this.letters.indexOf(message[i]) + this.letters.indexOf(keyStr[i]));
+        char = char >= 26 ? char % 26 : char;
+        decryptStr += this.letters[char];
+      } else {
+        decryptStr += message[i];
+      }
+    }
+    if(this.isRevert) {
+      return decryptStr.split('').reverse().join('');
+    } else return decryptStr;
+    
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(message, key) {
+    key = key.toUpperCase();
+    let keyStr = '';
+    let count = 0;
+    for(let j=0; j<message.length; j++) {
+      if(this.letters.indexOf(message[j])!==-1) {
+        keyStr += key[count];
+        count++;
+      } else {
+        keyStr += message[j];
+      }
+      if(count===key.length) {
+        count = 0;
+      }
+    }
+    let decryptStr = '';
+    for(let i=0; i<message.length; i++) {
+      let char;
+      if(this.letters.indexOf(message[i])!==-1) {
+        char = (this.letters.indexOf(message[i]) + 26 - this.letters.indexOf(keyStr[i]));
+        char = char >= 26 ? char % 26 : char;
+        decryptStr += this.letters[char];
+      } else {
+        decryptStr += message[i];
+      }
+    }
+    if(this.isRevert) {
+      return decryptStr.split('').reverse().join('');
+    } else return decryptStr;
   }
 }
 
